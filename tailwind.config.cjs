22f22ddffd3e4a57345537726e03108f45837604
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-11-06 20:33:20 -0300

    style: add grid cols custom values

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,15 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      gridTemplateColumns: {
+        mobile: "repeat(auto-fit, minmax(175px, 1fr))",
+        desktop: "repeat(auto-fit, minmax(225px, 1fr))",
+      },
       colors: {
         "vl-gray-light": "hsl(0, 0%, 98%)", // Light Mode Background
         "vd-blue-light": "hsl(200, 15%, 8%)", // Light Mode Text
         "d-gray-light": "hsl(0, 0%, 52%)", // Light Mode Input
+
         "vd-blue-dark": "hsl(207, 26%, 17%)", // Dark Mode Background
         white: "hsl(0, 0%, 100%)", // Dark Mode Text & Light Mode Elements
         "d-blue-dark": "hsl(209, 23%, 22%)", // Dark Mode Elements
